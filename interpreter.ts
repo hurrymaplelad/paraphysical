627e379352e0e59d7e7208de70b300b2b94b56b2
Adam Hull <adam@hmlad.com>
2022-10-06 22:54:47 -0700

    Parse statement labels

--- interpreter.ts
+++ interpreter.ts
@@ -1,10 +1,73 @@
-export default class Interpreter {
-  load(filename: string, contents: string): void {
+type Statement = Readonly<
+  & {
+    // The authored line number at the start of each line
+    label: number;
+    // The implicit line number in source code, not the authored line number prefix
+    sourceLineNumber: number;
+    filename: string;
+  }
+  & (
+    {
+      type: "comment";
+      comment: string;
+    } | {
+      type: "assignment";
+    }
+  )
+>;
 
+function makeParseError(
+  message: string,
+  { filename, sourceLineNumber }: Readonly<
+    { filename: string; sourceLineNumber: number }
+  >,
+): Error {
+  return new Error(
+    `Parse Error: ${message} [${filename}:${sourceLineNumber}]`,
+  );
+}
+
+function parseLine(
+  line: string,
+  context: Readonly<{ filename: string; sourceLineNumber: number }>,
+): Statement {
+  // Line label
+  const match = /^([0-9]{5})\s+(.*)$/.exec(line);
+  if (match == null) {
+    throw makeParseError("Line must start with 5 digit label", context);
+  }
+  const [_, labelString, rest] = match;
+  const label = parseInt(labelString, 10);
+
+  return { type: "comment", comment: rest, label, ...context };
+}
+
+type ParsedFile = Map<number, Statement>;
+
+function parseFile(
+  contents: string,
+  context: Readonly<{ filename: string }>,
+): ParsedFile {
+  const parsed = new Map();
+  for (const [i, line] of contents.split("\n").entries()) {
+    const statement = parseLine(line, { ...context, sourceLineNumber: i + 1 });
+    parsed.set(statement.label, statement);
+  }
+  return parsed;
+}
+
+export class Interpreter {
+  #files: { [key: string]: ParsedFile };
+
+  constructor() {
+    this.#files = {};
+  }
+
+  load(filename: string, contents: string): void {
+    this.#files[filename] = parseFile(contents, { filename });
   }
 
   run(filename: string): void {
-
   }
 
   getLocal(name: string): unknown {
--- tests/interpreter.test.ts
+++ tests/interpreter.test.ts
@@ -1,15 +1,26 @@
-import Interpreter from "../interpreter.ts";
+import { Interpreter } from "../interpreter.ts";
 import { expect } from "https://deno.land/x/expect@v0.2.10/mod.ts";
 
 async function readExampleText(filename: string): Promise<string> {
   return await Deno.readTextFile(`examples/${filename}`);
 }
 
-Deno.test("Interpreter", async () => {
+Deno.test("Interpreter", async (t) => {
   const interpreter = new Interpreter();
-  const filename = "hello.ppcl";
-  interpreter.load(filename, await readExampleText(filename));
-  interpreter.run(filename);
 
-  expect(interpreter.getLocal("out")).toEqual(1);
+  await t.step("evaluate a simple file", async () => {
+    const filename = "hello.ppcl";
+    interpreter.load(filename, await readExampleText(filename));
+    interpreter.run(filename);
+
+    expect(interpreter.getLocal("out")).toEqual(1);
+  });
+
+  await t.step("throw parsing an invalid file", async () => {
+    const filename = "invalid-line-number.ppcl";
+    const contents = await readExampleText(filename);
+    expect(() => interpreter.load(filename, contents)).toThrow(
+      /Parse Error.*invalid-line-number.ppcl:2/,
+    );
+  });
 });
